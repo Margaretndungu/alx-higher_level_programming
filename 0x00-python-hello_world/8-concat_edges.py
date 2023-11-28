9d0f7954c5a2cbbf8db782a65930edc24c22f0b8
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-11-27 23:08:03 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+print(str.split(',')[2].split()[4] + " " + str.split(',')[3].split()[0] + " " + str.s[plit(',')[3].split()[1])
+
 
