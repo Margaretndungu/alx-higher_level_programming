5ff72a96ad8f242fe7c588ffce597d39a7bbb67f
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-11-29 10:31:55 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,2 +1,7 @@
 def islower(c):
-    return 97 <= ord(c) <= 122
+
+    return ord('a') <= ord(c) <= ord('z') if len(c) == 1 else False
+
+print(islower('a'))
+print(islower('B'))
+print(islower('5'))
