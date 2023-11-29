fd8fb72b4729948d46417653cc16a161e993f9c1
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-11-29 05:29:44 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+output = ''
+
 for i in range(97, 123):
     if i != 101 and i != 113:
-        print(chr(i), end='')
+        output += chr(i)
+
+
+print("{}".format(output), end='')
